aa0b79e5c46fa1c13036b745ed7c8ec525901073
Mahesh Bhatt <c-bhattm@kapsarc.org>
2018-08-29 16:53:15 +0530

    test

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,22 +1,73 @@
-const {buildSchema} = require('graphql');
+const {
+  buildSchema,
+  GraphQLObjectType, 
+  GraphQLString, 
+  GraphQLSchema,
+  GraphQLNonNull
+ } = require('graphql');
 const graphqlHTTP = require('express-graphql');
 
 module.exports = (app) => {
 
-  const graphQLSchema = buildSchema(`
-type Query {
-  hello: String
-}`)
+//   const graphQLSchema = buildSchema(`
+//   type Query {
+//   quoteOfTheDay: String
+//   random: Float!
+//   rollDice(numDice: Int!, numSides: Int!): [Int] }
 
-  const root = {
-    hello: () => {
-      return 'Hello World';
-    },
-  };
+  
+// `)
+
+//   const root = {
+//     quoteOfTheDay: () => {
+//       return Math.random()<0.5 ? 'Take it easy': 'Salvation lies within'
+//     },
+//     random: () => {
+//       return Math.random()
+//     },
+//     rollDice: function (args) {
+//       var output = [];
+//       for (var i = 0; i < args.numDice; i++) {
+//         output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+//       }
+//       return output;
+//     }
+//   };
+
+var fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
+  },
+  'b': {
+    id: 'b',
+    name: 'bob',
+  },
+};
+
+var userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }
+});
+
+ var queryType = new GraphQLObjectType ({
+   name: 'Query',
+   fields: {
+     user: { type: userType,
+     args: { id : { type: new GraphQLNonNull(GraphQLString)}},
+     resolve: function(_, {id}){ return fakeDatabase[id]}
+     }
+   }
+ })
+
+ var schema  = new GraphQLSchema({ query: queryType })
 
   app.use('/graphql', graphqlHTTP({
-    schema: graphQLSchema,
-    rootValue: root,
+    schema: schema,
+    // rootValue: root,
     graphiql: true,
   }));
   app.listen(4000);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const mongoose = require('./config/mongoose');
+import express  from "express";
+import mongoose from './config/mongoose';
 const cors = require("cors");
 const db = mongoose();
 const app = express();
